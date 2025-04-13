cd69283370b5e81db3ad3793a117ab2f77ec8b54
KM-1992 <kazu.1992.august@gmail.com>
2025-04-13 14:02:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title("🎈Hello World")
 st.snow()
 st.title('title string `<h1>`')
+st.header('大見出し `<h2>`')
+st.subheader(body='中見出し `<h3>`',
+            anchor='title',
+            help='`<h3>`あるいは`###`に相当するstreamlitのコマンド',
+            divider=True)
